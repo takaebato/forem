b546f0bfdff59f656fe42ef5ed4597880f2dedfa
Ridhwana <ridhwana.khan16@gmail.com>
2022-05-01 13:14:58 +0200

    feat: move spec

--- spec/requests/admin/users/invitations_spec.rb
+++ spec/requests/admin/users/invitations_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "/admin/invitations", type: :request do
+RSpec.describe "/admin/users/invitations", type: :request do
   let(:user) { create(:user) }
   let(:admin) { create(:user, :super_admin) }
 
@@ -12,14 +12,14 @@ RSpec.describe "/admin/invitations", type: :request do
   describe "GET /admin/invitations" do
     it "renders to appropriate page" do
       user.update_column(:registered, false)
-      get admin_invitations_path
+      get admin_users_invitations_path
       expect(response.body).to include(user.username)
     end
   end
 
   describe "GET /admin/invitations/new" do
     it "renders to appropriate page" do
-      get new_admin_invitation_path
+      get new_admin_users_invitation_path
       expect(response.body).to include("Email")
       expect(response.body).to include("Name")
     end
@@ -27,14 +27,14 @@ RSpec.describe "/admin/invitations", type: :request do
 
   describe "POST /admin/invitations" do
     it "creates new invitation" do
-      post admin_invitations_path,
+      post admin_users_invitations_path,
            params: { user: { email: "hey#{rand(1000)}@email.co", name: "Roger #{rand(1000)}" } }
       expect(User.last.registered).to be false
     end
 
     it "enqueues an invitation email to be sent", :aggregate_failures do
       assert_enqueued_with(job: Devise.mailer.delivery_job) do
-        post admin_invitations_path,
+        post admin_users_invitations_path,
              params: { user: { email: "hey#{rand(1000)}@email.co", name: "Roger #{rand(1000)}" } }
       end
 
@@ -43,7 +43,7 @@ RSpec.describe "/admin/invitations", type: :request do
 
     it "does not create an invitation if a user with that email exists" do
       expect do
-        post admin_invitations_path,
+        post admin_users_invitations_path,
              params: { user: { email: admin.email, name: "Roger #{rand(1000)}" } }
       end.not_to change { User.all.count }
       expect(admin.reload.registered).to be true
@@ -56,7 +56,7 @@ RSpec.describe "/admin/invitations", type: :request do
 
     it "enqueues an invitation email to be resent" do
       assert_enqueued_with(job: Devise.mailer.delivery_job) do
-        post resend_admin_invitation_path(invitation.id)
+        post resend_admin_users_invitation_path(invitation.id)
       end
       expect(enqueued_jobs.last[:args]).to match(array_including("invitation_instructions"))
     end
@@ -71,9 +71,9 @@ RSpec.describe "/admin/invitations", type: :request do
 
     it "deletes the invitation" do
       expect do
-        delete admin_invitation_path(invitation.id)
+        delete admin_users_invitation_path(invitation.id)
       end.to change { User.all.count }.by(-1)
-      expect(response.body).to redirect_to admin_invitations_path
+      expect(response.body).to redirect_to admin_users_invitations_path
     end
   end
 end
