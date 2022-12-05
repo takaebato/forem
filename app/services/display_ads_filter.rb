122d0eac319cdfbeb8f5215550d7352d6a618ab3
Ridhwana <ridhwana.khan16@gmail.com>
2022-12-05 16:20:57 +0200

    refactor: create steps that indicate how the filtering is done

--- app/services/display_ads_filter.rb
+++ app/services/display_ads_filter.rb
@@ -10,24 +10,18 @@ class DisplayAdsFilter
   end
 
   def call
-    relation = DisplayAd.approved_and_published.where(placement_area: area).order(success_rate: :desc)
+    # the order is called at the bottom and so we can remove it
+    relation = filter_by_approved_and_published.order(success_rate: :desc)
 
     if article_tags.any?
-      display_ads_with_no_tags = relation.where(cached_tag_list: "")
-      display_ads_with_targeted_article_tags = relation.cached_tagged_with_any(article_tags)
-
-      relation = display_ads_with_no_tags.or(display_ads_with_targeted_article_tags)
+      relation = filter_by_article_tags(relation)
     end
 
     if article_tags.blank?
-      relation = relation.where(cached_tag_list: "")
+      relation = filter_in_articles_with_no_tags(relation)
     end
 
-    relation = if user_signed_in
-                 relation.where(display_to: %w[all logged_in])
-               else
-                 relation.where(display_to: %w[all logged_out])
-               end
+    relation = filter_by_user_sign_in(relation)
 
     relation.order(success_rate: :desc)
 
@@ -41,4 +35,28 @@ class DisplayAdsFilter
   private
 
   attr_reader :area, :user_signed_in, :article_tags
+
+  def filter_by_approved_and_published
+    DisplayAd.approved_and_published.where(placement_area: area)
+  end
+
+  def filter_by_article_tags(relation)
+    # relation.where(cached_tag_list: "") can be possibly moved out
+    display_ads_with_no_tags = relation.where(cached_tag_list: "")
+    display_ads_with_targeted_article_tags = relation.cached_tagged_with_any(article_tags)
+
+    display_ads_with_no_tags.or(display_ads_with_targeted_article_tags)
+  end
+
+  def filter_in_articles_with_no_tags(relation)
+    relation.where(cached_tag_list: "")
+  end
+
+  def filter_by_user_sign_in(relation)
+    if user_signed_in
+      relation.where(display_to: %w[all logged_in])
+    else
+      relation.where(display_to: %w[all logged_out])
+    end
+  end
 end
